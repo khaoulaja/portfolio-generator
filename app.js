ba5026555d48755dea57bbe2d2ef9061ec31c7ce
khaoula <khaoulajabour4@gmail.com>
2021-12-01 03:18:41 -0500

    generate the html file

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
+const fs =require('fs');     
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length)
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;                                                                                               
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]); 
-    }
 
-    console.log("=======================");
-profileDataArr.forEach((profileItem) =>{
-    console.log(profileItem);
+fs.writeFile('index.html', generatePage(name,github), err => {
+  if(err) throw err;
+  console.log('Portfolio complete! check out index.html to see the output!');
 });
-}
-printProfileData(profileDataArgs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(profileDataArgs);
+
+// const printProfileData = (profileDataArr) => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]); 
+//     }
+
+//     console.log("=======================");
+// profileDataArr.forEach((profileItem) =>{
+//     console.log(profileItem);
+// });
+// }
+// printProfileData(profileDataArgs);
