6875ca2872a2645ca50f08037f822258a09b36e3
khaoula <khaoulajabour4@gmail.com>
2021-12-03 20:48:09 -0500

    create header footer and about sections

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const inquirer =require('inquirer'); 
-// const fs =require('fs');    
-// const generatePage = require('./src/page-template.js');
+const fs =require('fs');    
+const generatePage = require('./src/page-template.js');
 
 // //const profileDataArgs = process.argv.slice(2, process.argv.length)
 // //const [name, github] = profileDataArgs;   
-// const pageHTML = generatePage(name, github);
 
-
-// fs.writeFile('index.html',pageHTML, err => {
-//   if(err) throw err;
-//   console.log('Portfolio complete! check out index.html to see the output!');
-//});
 const promptUser = ()=>{
 return inquirer.prompt([
     {
@@ -139,7 +133,13 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
+  const pageHTML = generatePage(portfolioData);
+
+fs.writeFile('index.html',pageHTML, err => {
+  if(err) throw err;
+  console.log('Portfolio complete! check out index.html to see the output!');
+});
+ 
 });
 
 
